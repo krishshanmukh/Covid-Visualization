827f8c52d802bd1732572c227383a5bfe0477520
Saurav Singh <sdsingh98@gmail.com>
2020-05-12 04:35:26 -0400

    Changed donut acc to state

--- map.js
+++ map.js
@@ -26,7 +26,7 @@ var legendText = ["0", "", "10", "", "1000", "", "5000", ""];
 var legendColors = ["#fff7bc", "#fee391", "#fec44f", "#fe9929", "#ec7014", "#cc4c02", "#993404", "#662506"];
 
 
-function ready(error, data, us,) {
+function ready(error, data, us) {
 	console.log("Data",data);
 
 	var counties = topojson.feature(us, us.objects.counties);
@@ -79,7 +79,20 @@ function ready(error, data, us,) {
 			tooltip.transition()
 			.duration(250)
 			.style("opacity", 0);
+		})
+		.on("click", function(d) {
+			console.log("Click",d);
+			select_state(data, d.properties.date[0]["Province_State"]);
 		});
+
+		function select_state(data, state){
+			// console.log("State1",data);
+			statewise = data.filter(function(d){return d["Province_State"] == state;})
+			console.log("State",statewise);
+			d3.selectAll("#donut").remove();
+			donutchart(statewise);
+			return statewise;
+		}
 
 	svg.append("path")
 		.datum(topojson.feature(us, us.objects.states, function(a, b) { return a !== b; }))
@@ -138,84 +151,90 @@ function ready(error, data, us,) {
 			});
 
 	update(1);
+	donutchart(data);
+	function donutchart(data){
+		var margin = {top: 20, right: 20, bottom: 20, left: 20};
+	width = 800 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom,
+    formatPercent = d3.format(".1%");
+		var
+	    radius = Math.min(width, height) / 2;
 
+		var color1 = d3.scale.category20();
 
-	var
-    radius = Math.min(width, height) / 2;
+		var pie = d3.layout.pie()
+		    .value(function(d) { return d["1"]; })
+		    .sort(null);
 
-	var color1 = d3.scale.category20();
+		var arc = d3.svg.arc()
+		    .innerRadius(radius - 100)
+		    .outerRadius(radius - 20);
 
-	var pie = d3.layout.pie()
-	    .value(function(d) { return d["1"]; })
-	    .sort(null);
+		var svgpie = d3.select("body").append("svg")
+		    .attr("width", width)
+		    .attr("height", height)
+		    .attr("id","donut")
+		  .append("g")
+		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-	var arc = d3.svg.arc()
-	    .innerRadius(radius - 100)
-	    .outerRadius(radius - 20);
+		d3.csv("covid_data_processed.csv", type, function(error, data) {});
+			var slide = d3.select("#slide")
+				.on("input", function() {
+						day = this.value;
+						// console.log(day);
+						update(day);
+						change(data,day);
+					});
 
-	var svgpie = d3.select("body").append("svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	  .append("g")
-	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+		  var path = svgpie.datum(data).selectAll("path")
+		      .data(pie)
+		    .enter().append("path")
+		      .attr("fill", function(d, i) { return color1(i); })
+		      .attr("d", arc)
+		      .each(function(d) { this._current = d; }); // store the initial angles
 
-	d3.csv("covid_data_processed.csv", type, function(error, data) {
-		var slide = d3.select("#slide")
-			.on("input", function() {
-					day = this.value;
-					// console.log(day);
-					update(day);
-					change(data,day);
+		path
+			.on("mouseover", function(d) {
+				// console.log("D",d);
+					tooltip.transition()
+					.duration(250)
+					.style("opacity", 1);
+					tooltip.html(
+					"<p><strong>" + d.data["Admin2"] + "</strong></p>" +
+					"<table><tbody><tr><td class='wide'>Cases on Day " + day +":</td><td>" + d.data[day] + "</td></tr>" +
+					"</tbody></table>"
+					)
+					.style("left", (d3.event.pageX + 15) + "px")
+					.style("top", (d3.event.pageY - 28) + "px");
+				})
+				.on("mouseout", function(d) {
+					tooltip.transition()
+					.duration(250)
+					.style("opacity", 0);
 				});
 
-	  var path = svgpie.datum(data).selectAll("path")
-	      .data(pie)
-	    .enter().append("path")
-	      .attr("fill", function(d, i) { return color1(i); })
-	      .attr("d", arc)
-	      .each(function(d) { this._current = d; }); // store the initial angles
+		  function change(data,day) {
+		    pie.value(function(d) {return d[day]; }); // change the value function
+		    path = path.data(pie); // compute the new angles
+		    path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
+		  }
+		// });
 
-	path
-		.on("mouseover", function(d) {
-			// console.log("D",d);
-				tooltip.transition()
-				.duration(250)
-				.style("opacity", 1);
-				tooltip.html(
-				"<p><strong>" + d.data["Admin2"] + "</strong></p>" +
-				"<table><tbody><tr><td class='wide'>Cases on Day " + day +":</td><td>" + d.data[day] + "</td></tr>" +
-				"</tbody></table>"
-				)
-				.style("left", (d3.event.pageX + 15) + "px")
-				.style("top", (d3.event.pageY - 28) + "px");
-			})
-			.on("mouseout", function(d) {
-				tooltip.transition()
-				.duration(250)
-				.style("opacity", 0);
-			});
-
-	  function change(data,day) {
-	    pie.value(function(d) {return d[day]; }); // change the value function
-	    path = path.data(pie); // compute the new angles
-	    path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
-	  }
-	});
-
-	function type(d) {
-		for (i = 1; i < 81; i++) {
-		  d[i] = +d[i];
+		function type(d) {
+			for (i = 1; i < 81; i++) {
+			  d[i] = +d[i];
+			}
+		  return d;
 		}
-	  return d;
-	}
 
 
-	function arcTween(a) {
-	  var i = d3.interpolate(this._current, a);
-	  this._current = i(0);
-	  return function(t) {
-	    return arc(i(t));
-	  };
+		function arcTween(a) {
+		  var i = d3.interpolate(this._current, a);
+		  this._current = i(0);
+		  return function(t) {
+		    return arc(i(t));
+		  };
+		}
 	}
 }
 
